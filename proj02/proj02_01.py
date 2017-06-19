e4d75d0712ae1dbb0ec70b01acd11cb3a38775a6
VanathiSelvan <vanathi.selvan@outlook.com>
2017-06-19 15:34:11 -0500

    Add files via upload

--- proj02/proj02_01.py
+++ proj02/proj02_01.py
@@ -1,22 +1,21 @@
 # Name:
 # Date:
 
-# proj01: A Simple Program
-# This program asks the user for his/her name and age.
-# Then, it prints a sentence that says when the user will turn 100.
 
-# If you complete extensions, describe your extensions here!
 
 name = raw_input("Enter your name: ")
 age = int(raw_input("Enter your age: "))
 birthday = raw_input("Has your birthday happened this year? Enter Y or N: ")
 
 if birthday == "Y":
-    # Calculates the year that the user will be 100
     year_100 = str((100 - age) + 2017)
 
 else:
-    # Calculates the year that the user will be 100
     year_100 = str((100 - age) + 2016)
+
+while age < 101:
+    print
+    year = year + 1
+
 
 print name, " will turn 100 in the year ", year_100, "."
