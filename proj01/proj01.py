a2cebf5757b736d03651708d314cd8eb4a2c0cd5
VanathiSelvan <vanathi.selvan@outlook.com>
2017-06-19 15:18:19 -0500

    Add files via upload

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -16,29 +16,56 @@ print name
 age = raw_input("Enter your age: ")
 age_int = int(age)
 
-print age
-print age_int
+birthday = raw_input("Has your birthday already been completed?: ")
+if birthday == "yes":
+    age_int_hundred = (100 - age_int)
+    print "That's amazing"
+    print name, "You will be 100 years old in ", age_int_hundred, "years."
+    print age
+    print age_int_hundred
+else:
+    age_int_hundred = (101 - age_int)
+    print "I hope you are excited for your birthday soon!"
+    print name, "You will be 100 years old in ", age_int_hundred, "years."
+
+movie = raw_input ("What type of movie are you interested in or allowed to watch?: ")
+if movie == "G":
+    print "Select Movies in Genre G"
+
+
+if movie == "PG":
+    print "Select Movies in Genre PG"
+
+
+if movie  == "PG-13":
+    if age_int >= 13:
+        print "allow"
+    if age_int <= 13:
+        print "Denied"
+        permission = raw_input ("What type of movie are you interested in or allowed to watch?: ")
+        print "Go back to Main Menu or use Parents/Guardians Touch ID"
+
+if movie == "R":
+    print age_int
+    if age_int > 17:
+        print "Select Movies in Genre R"
+    if age_int <= 17:
+        print "Denied"
+
+
+
+
+
+
+
+
 
 year = raw_input("Enter your year: ")
-year_int = int(age)
+year_int = age_int
 
-print year
-print year_int
-
-age_int = (100 - age)
-
-year = age + year
+print name, "You will be 100 years old in ", age_int, " years."
 
 
-
-
-
-
-#raw_input("Enter your age: ")
-#x = 2017
-#x = int(x)
-#x + 100
-#x = 100 - "Enter your age"
 
 
 
