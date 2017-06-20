5ab4de80d27aaeb3cdbb993a748eff091699aace
VanathiSelvan <vanathi.selvan@outlook.com>
2017-06-20 15:25:55 -0500

    Add files via upload

--- proj04/proj04.py
+++ proj04/proj04.py
@@ -7,4 +7,26 @@ proj04
 Asks the user for a string and prints out whether or not the string is a palindrome.
 
 """
+newList = newList[:3] + [7] + newList[3:]
+print newList
+
+
+
+
+user_input= raw_input("What do you think is a palindrome?: ")
+
+user_input_list = []
+for letter in user_input:
+    user_input_list.append(letter)
+print user_input_list
+
+reverse = []
+
+length = len(newList)
+print length
+
+
+
+
+
 
